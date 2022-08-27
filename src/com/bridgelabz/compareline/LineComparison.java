dc1de794d60f7ac01ae413b8e363a7914af04adf
Pallavic5 <pchedge235@gmail.com>
2022-08-27 19:08:39 +0530

    first commit

---
+++ src/com/bridgelabz/compareline/LineComparison.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.compareline;
+
+public class LineComparison {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to Line Comparison Computation Program.");
+
+	}
+
+}
