ae6f132eec673500c2e3da859e31b1b2d18e62b1
Pallavic5 <pchedge235@gmail.com>
2022-08-27 19:30:53 +0530

    Added: Check equality of lines using equals method

--- src/com/bridgelabz/compareline/LineComparison.java
+++ src/com/bridgelabz/compareline/LineComparison.java
@@ -8,10 +8,9 @@ public class LineComparison {
 		
 		System.out.println("Welcome to Line Comparison Computation Program.");
 		
-		Scanner sc =new Scanner(System.in);
-		
-        double x1,x2,y1,y2=0;
-        int lengthOfLine=0;
+		Scanner sc = new Scanner(System.in);
+        double x1,x2,y1,y2,x3,y3,x4,y4 = 0;
+        int line1,line2=0;
         
         System.out.println("Enter x1 & y1 values of First point");
         x1=sc.nextDouble();
@@ -21,9 +20,32 @@ public class LineComparison {
         x2=sc.nextDouble();
         y2=sc.nextDouble();
         
-        lengthOfLine=(int)Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
-        System.out.println("Length of a Line = "+ lengthOfLine);
+        line1=(int)Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+        System.out.println("Length of First line = "+line1);
+       
+        System.out.println("Enter x3 & y3 values of Third point");
+        x3=sc.nextDouble();
+        y3=sc.nextDouble();
+        
+        System.out.println("Enter x4 & y4 values of Fourth point");
+        x4=sc.nextDouble();
+        y4=sc.nextDouble();
+        
+        line2=(int)Math.sqrt((x4 - x3) * (x4 - x3) + (y4 - y3) * (y4 - y3));
+        System.out.println("Length of Second line = "+line2);
+        
+        String str1 = Double.toString(line1);
+        String str2 = Double.toString(line2);
+        
 
+        if(str1.equals(str2) == true) {
+        	
+        	System.out.println("Both lines are equal");
+        }
+        else {
+        	
+        	System.out.println("Both lines are not equal");
+        }
 	}
 
 }
